b43c23c3f0929776f1264caf4a5df1f581b3085d
Nihed <nihed.hajlaoui@gmail.com>
2021-12-21 18:51:10 +0100

    un essai

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ecommerce';
-  students = [
-    {
-      id:1,
-      name:"yassine",
-      section:'if4'
-    },
-    {
-      id:4,
-      name:"BILEL",
-      section:'if4'
-    }
-  ]
+  title = 'ANNOU Alaways-Valid';
+  
+
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
-import { JjjComponent } from './jjj/jjj.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { EnTeteComponent } from './en-tete/en-tete.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
-    JjjComponent
+    NavBarComponent,
+    EnTeteComponent,
+  
   ],
   imports: [
     BrowserModule,
